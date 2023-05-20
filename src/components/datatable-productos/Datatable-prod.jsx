233f7cb406525a54c43cc8db92e02b2233a247d6
Andres <andresgow1@gmail.com>
2023-05-19 22:14:03 -0500

    first commit

---
+++ src/components/datatable-productos/Datatable-prod.jsx
@@ -0,0 +1,91 @@
+import "./datatable.scss";
+import { DataGrid } from "@mui/x-data-grid";
+import { productsColumns, productsRows } from "../../datatablesource";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import {db} from "../../firebase"
+import { doc, deleteDoc } from "firebase/firestore";
+
+
+
+const Datatable = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(()=>{
+    const fetchData = async()=>{
+        let list = []
+        try{
+            const querySnapshot = await getDocs(collection(db, "products"));
+            querySnapshot.forEach((doc) => {
+              // doc.data() is never undefined for query doc snapshots
+              list.push({id: doc.id, ...doc.data()})
+              console.log(doc.id, " => ", doc.data());
+            });
+            setData(list)
+
+            console.log(list)
+        }catch(error){
+            console.log(error)
+        }
+       
+    }
+    fetchData()
+  },[])
+
+  console.log(data)
+
+  const handleDelete = async(id) => {
+    try{
+        await deleteDoc(doc(db, "products", id));
+        setData(data.filter((item) => item.id !== id));
+    }catch(error){
+        console.log(error)
+    }
+ 
+    
+  };
+
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="cellAction">
+            <Link to="/users/test" style={{ textDecoration: "none" }}>
+              <div className="viewButton">View</div>
+            </Link>
+            <div
+              className="deleteButton"
+              onClick={() => handleDelete(params.row.id)}
+            >
+              Delete
+            </div>
+          </div>
+        );
+      },
+    },
+  ];
+  return (
+    <div className="datatable">
+      <div className="datatableTitle">
+       Agregar Nuevos Produtos
+        <Link to="/products/newProduct" className="link">
+          Nuevo
+        </Link>
+      </div>
+      <DataGrid
+        className="datagrid"
+        rows={data}
+        columns={productsColumns.concat(actionColumn)}
+        pageSize={9}
+        rowsPerPageOptions={[9]}
+        checkboxSelection
+      />
+    </div>
+  );
+};
+
+export default Datatable;
---
+++ src/pages/newProduct/NewProduct.jsx
@@ -0,0 +1,94 @@
+import "./new.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
+import { useState } from "react";
+import { doc, setDoc } from "firebase/firestore";
+import {db} from "../../firebase"
+import { collection, addDoc } from "firebase/firestore";
+import { TextField } from "@mui/material";
+
+
+
+
+
+const New = ({ inputs, title }) => {
+  const [file, setFile] = useState("");
+
+  const [nombre, setNombre] = useState("");
+  const [precioCompra, setPrecioCompra] = useState("");
+  const [precioVenta, setPrecioVenta] = useState("");
+  const [stock, setStock] = useState("");
+
+  const handleAdd = async(e)=>{
+    
+    e.preventDefault()
+    const docRef = await addDoc(collection(db, "products"), {
+      nombre: nombre,
+      precio_compra: precioCompra,
+      precio_venta: precioVenta,
+      stock: stock
+      
+    });
+    
+
+    
+  }
+
+
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>{title}</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <TextField
+              label="Nombre"
+              color="primary"
+              focused
+              onChange={(e) => setNombre(e.target.value)}
+              margin="dense"
+            />
+          </div>
+          <div className="right">
+            <form onSubmit={handleAdd}>
+              <div className="formInput">
+                <TextField
+                
+                  color="primary"
+                  focused
+                  label="Precio Compra"
+                  margin="dense"
+                  onChange={(e) => setPrecioCompra(e.target.value)}
+                />
+                <TextField
+                  color="primary"
+                  focused
+                  margin="dense"
+                  label="Precio Venta"
+                  onChange={(e) => setPrecioVenta(e.target.value)}
+                />
+              </div>
+
+              <TextField
+              margin="dense"
+                color="primary"
+                focused
+                label="Stock"
+                onChange={(e) => setStock(e.target.value)}
+              />
+
+              <button type="submit">Agregar Producto</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default New;
